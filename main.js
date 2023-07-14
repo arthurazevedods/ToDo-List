ed14bef9777caf7bef4fc193f003808eedebb241
Arthur Silva <arthurazevedods@gmail.com>
2023-07-14 14:56:00 -0300

    reorganize divs

--- main.js
+++ main.js
@@ -40,30 +40,38 @@ apagar_tudo.addEventListener("click", function(ev){
 /*A tarefa vai ter o nome, descrição e um botão */
 function criaTarefa(item){
     
-    const novaTarefa = document.createElement('li');
+    const novaTarefa = document.createElement('div');
     novaTarefa.classList.add("item");
     
     
-    const liItem = document.createElement('li');
-    const liTarefa = document.createElement('span'); //cria um elemento
-    liTarefa.classList.add('tarefa');
-    liTarefa.innerHTML = item.tarefa;
+    const divItem = document.createElement('div');
+    const divTarefa = document.createElement('span'); //cria um elemento
+    divTarefa.classList.add('tarefa');
+    divTarefa.innerHTML = item.tarefa;
 
-    const liDescricao = document.createElement('span');
-    liDescricao.classList.add('descricao');
-    liDescricao.classList.add('hidden');
-    liDescricao.innerHTML = item.descricao;
+    const container_Desc = document.createElement('div');
+    container_Desc.classList.add('container_desc');
+    container_Desc.classList.add('hidden');
+    const divDescricao = document.createElement('span');
+    divDescricao.classList.add('descricao');
+    //divDescricao.classList.add('hidden');
+    divDescricao.innerHTML = item.descricao;
 
-    liItem.append(liTarefa);
-    liItem.append(liDescricao);
+    divItem.append(divTarefa);
+    //divItem.append(divDescricao);
 
     createCheckButton(novaTarefa);
-    novaTarefa.append(liItem);
+    novaTarefa.append(divItem);
     
-    createInfoButton(novaTarefa)
-    createDeleteButton(novaTarefa);
+    /*DIV-BTNS */
+    const div_btns = document.createElement('div_btns');
+    div_btns.classList.add('div_btns');
+    createInfoButton(div_btns);
+    createDeleteButton(div_btns);
+    novaTarefa.append(div_btns);
     
-    
+    container_Desc.append(divDescricao);
+    novaTarefa.append(container_Desc);
 
     lista.append(novaTarefa);
 }
@@ -80,24 +88,24 @@ function createCheckButton(context) {
     btn.style.visibility = 'hidden';
     btn_check.addEventListener('click',function(){
         console.log(btn)
-        //console.log(btn_check.nextSibling);
+        
         const elm = btn_check.nextSibling;
-        //console.log(elm.querySelector('span'));
+        
         var task = elm.querySelector('span');
-        //console.log(elm.querySelector('.descricao'));
+        
         var desc = elm.querySelector('.descricao');
         if (task.style.textDecoration === "line-through"){
             task.style.textDecoration = "none";
             desc.style.textDecoration = "none";
             btn.style.visibility = 'hidden';
-            //btn_check.style.boxShadow = '0px 0.2rem #FDB235'
+            
         }else{
             task.style.textDecoration = "line-through";
             desc.style.textDecoration = "line-through";
-            //btn_check.style.backgroundColor = 'green';
+            
             btn.style.visibility = 'visible';
             btn.style.color = '#FDB235';
-            //btn_check.style.boxShadow = '1px 0.1rem #FDB235'
+            
         }
         
     })
@@ -116,12 +124,11 @@ function createInfoButton(context){
     div_info.append(btn_info);
 
     div_info.addEventListener('click',function(){
-        console.log(btn_info);
-        var prev = div_info.previousSibling;
-        console.log(prev.querySelector('.descricao'));
-        prev = prev.querySelector('.descricao');
-        prev.classList.toggle('hidden');//adiciona a classe se não existir e, caso contrário, remove
-        
+        var prev = div_info.parentElement.parentElement;
+        console.log(div_info.parentElement.parentElement);
+        console.log(prev.querySelector('.container_desc'));
+        prev = prev.querySelector('.container_desc');
+        prev.classList.toggle('hidden'); //adiciona a classe se não existir e, caso contrário, remove
     })
     
     context.appendChild(div_info);
@@ -139,13 +146,14 @@ function createDeleteButton(context){
     div_trash.append(btn_trash);
 
     div_trash.addEventListener('click',function(){
-        var tempo = 500;
-        var del_task = div_trash.parentNode;
-        del_task.style.transition = 'opacity 500ms ease';
+        var tempo = 400;
+        var del_task = div_trash.parentElement.parentElement;
+        del_task.style.transition = `opacity ${tempo}ms ease`;
+
         setTimeout(function(){
             removeItem(tarefas, del_task.querySelector('li'));
-            del_task.parentNode.removeChild(del_task);
-        }, 500);
+            del_task.parentElement.removeChild(del_task); //Retorna a div .item e apaga o elemento filho que é del_task
+        }, tempo);
         
         
     })
@@ -154,23 +162,12 @@ function createDeleteButton(context){
 }
 /*erroo */
 function removeItem(tasks, value) {
-    
-    
-    const tarefa = value.querySelector('.tarefa').innerHTML;
-    const descricao = value.querySelector('.descricao').innerHTML;
-    console.log(tarefa);
-    console.log(descricao);
+    const del_tarefa = value.querySelector('.tarefa').innerHTML;
 
-    const tarefaPLixo = {
-        "tarefa": tarefa,
-        "descricao": descricao
-    }
-    console.log('tarefaPLixo: '+tarefaPLixo);
     console.log('tarefas: '+tarefas);
-    var tar_json = JSON.stringify(tarefaPLixo);
-    console.log('tar_json: '+tar_json);
+    
     tarefas.forEach((item,index)=>{
-        if(item.tarefa === tarefaPLixo.tarefa){
+        if(item.tarefa === del_tarefa){
             console.log('item:'+item.tarefa+'\nE o index é:'+index);
             tarefas.splice(index,1);
             localStorage.setItem('tarefas', JSON.stringify(tarefas));
